7a5ff85b62989aff99e9c8055865c6b6f3cd8d8c
chldudms <yueun0337@gmail.com>
2025-04-01 09:46:21 +0900

    navigatie와 link를 이용해 페이지 이동 구현

--- src/pages/About.js
+++ src/pages/About.js
@@ -1,7 +1,21 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
 const About = () => {
-    return <h1>about 화면 입니앙.</h1>
+    return(
+
+    <div> 
+    <Link to="/">Home</Link>
+            <Link to="/Post">Post</Link>
+            <Link to="/Profile">Profile</Link> 
+        
+        
+        <h1>about 화면 입니앙.</h1>
+
+     </div>
+    );
+
+
 }
 
 export default About;
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,7 +1,25 @@
 import React from "react";
+import { useNavigate } from "react-router-dom";
 
 function Home() {
-    return <h1>Home Page</h1>;
+
+    const navigate = useNavigate();
+
+    const movepage = () => {
+        navigate("/About");
+    };
+
+    return(
+        <div>
+         <h1>DearPal</h1>
+         <h2>당신의 이야기를 편지로 남겨보세요.</h2>
+         <button onClick={movepage}>start</button>
+      </div>
+
+      
+    )
+    
 }
+
 
 export default Home;
