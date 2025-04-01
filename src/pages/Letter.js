c47c66471f5f72c5580ecc432d218cd1a6a512f4
chldudms <yueun0337@gmail.com>
2025-04-02 00:27:51 +0900

    useState

---
+++ src/pages/Letter.js
@@ -0,0 +1,27 @@
+import React from "react";
+
+function Letter(){
+   
+    return (
+        <div className="letter-container">
+            <div className="letter-paper">
+                <input
+                    type="text"
+                    placeholder="제목을 입력하세요"
+                    className="letter-title"
+                
+                />
+                <textarea
+                    placeholder="여기에 편지를 작성하세요..."
+                    className="letter-content"
+         
+                />
+                <button>
+                    편지 보내기
+                </button>
+            </div>
+        </div>
+    );
+}
+
+export default Letter;
