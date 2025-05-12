85716889af8098addb60c4a130a37ef249497f4b
chldudms <yueun0337@gmail.com>
2025-05-13 00:14:40 +0900

    백엔드 - 회원가입 api 구현

---
+++ server.js
@@ -0,0 +1,56 @@
+const express = require('express');
+const cors = require('cors'); //모든 출처 허용하는 미들웨어-프론트와 백 분리된 구조에서 API 호출
+const mysql = require('mysql2');
+const bcrypt = require('bcrypt'); //비밀번호 암호화해주는 라이브러리
+const app = express();
+const dotenv = require('dotenv'); //.env파일로 민감한 정보 이동
+
+dotenv.config(); 
+
+app.use(cors());
+app.use(express.json());
+
+
+const db = mysql.createConnection({
+    host: process.env.DB_HOST,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_NAME
+})
+
+
+db.connect(err => {
+    if (err) throw err;
+    console.log("MySQL 연결 완료");
+});
+
+//가입 api
+app.post('/join', async (req, res) => {
+    const { userName, userId, userPw} = req.body;
+    const seed = Math.random().toString(36).substring(2, 10); //프로필 시드
+
+    console.log("요청 바디:", req.body);
+
+    //중복된 userid 체크
+    db.query('SELECT * FROM user WHERE userid = ?', [userId], async (err, results) => {
+        if (results.length > 0) {
+            return res.json({ message: '이미 존재하는 아이디입니다.' });
+        }
+
+        const hashedPw = await bcrypt.hash(userPw, 10);  //비밀번호 해싱 (비밀번호,salt)
+        console.log(hashedPw)
+
+       //사용자 정보 DB에 저장
+        db.query('INSERT INTO user (userid, username, password, profile_seed) VALUES (?, ?, ?, ?)', [userId, userName, hashedPw, seed], (err, result) => {
+            if (err) {
+                console.error(err);
+                return res.status(500).json({ message: '회원가입 중 오류가 발생했습니다.' });
+            }
+            res.json({ message: '회원가입 성공' });
+        });
+    });
+});
+
+app.listen(5000, () => {
+    console.log('http://localhost:5000/join');
+});
--- src/pages/Join.js
+++ src/pages/Join.js
@@ -6,19 +6,44 @@ import "../styles/login.css" //login페이지와 같은 css사용
 
 function Join() {
     const navigate = useNavigate();
+    const [userName, setName] = useState();
     const [userId, setUserId] = useState();
     const [userPw, setUserPw] = useState();
     const [pwCheck, setPwCheck] = useState();
 
-    function gotoJoin(){
-        if(userPw==pwCheck){
-            window.alert("가입 성공")
-            navigate('/About')
+    function gotoJoin() {
+        if (userPw === pwCheck) {
+
+            console.log(userName)
+            fetch('http://localhost:5000/join', {
+                method: 'POST',
+                headers: {
+                    'Content-Type': 'application/json'
+                },
+                body: JSON.stringify({
+                    userName,
+                    userId,
+                    userPw
+                })
+            })
+                .then(res => res.json())
+                .then(data => {
+                    if (data.message === "회원가입 성공") {
+                        window.alert("가입 성공")
+                        navigate('/About');
+                    } else {
+                        window.alert(data.message);
+                    }
+                })
+                .catch(err => {
+                    console.error(err);
+                    window.alert("서버 오류 발생");
+                });
+        } else {
+            window.alert("비밀번호가 일치하지 않습니다. 다시 입력해주세요.");
         }
-       else{
-        window.alert("비밀번호가 일치하지 않습니다. 다시 입력해주세요.")
-       }
     }
+
 
     return ( 
         <div>
@@ -29,6 +54,8 @@ function Join() {
                 <h3>회원가입</h3>
 
                 <form className='Joginform'>
+                    <h5>nickname</h5>
+                    <input className='userid' value={userName} onChange={(e) => setName(e.target.value)} /> <br />
                     <h5>id</h5>
                     <input className='userid' value={userId} onChange={(e) => setUserId(e.target.value)} /> <br />
                     <h5>password</h5>
