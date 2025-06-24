dd802d24f5d559746126c5c315dc1f1502be0958
chldudms <yueun0337@gmail.com>
2025-06-24 16:03:06 +0900

    fix: 서버 성공 메세지 수정, 디비쿼리 비동기 방식으로 바꾸기

--- backend/db/db.js
+++ backend/db/db.js
@@ -1,19 +1,15 @@
-const mysql = require('mysql2');
-const dotenv = require('dotenv'); //.env파일로 민감한 정보 이동
+const mysql = require('mysql2/promise');
+const dotenv = require('dotenv');
+dotenv.config();
 
-dotenv.config();	//.env 파일을 읽어서 process.env에 등록
-
-const db = mysql.createConnection({
+const db = mysql.createPool({
     host: process.env.DB_HOST,
     user: process.env.DB_USER,
     password: process.env.DB_PASSWORD,
-    database: process.env.DB_NAME
-})
-
-
-db.connect(err => {
-    if (err) throw err;
-    console.log("MySQL 연결 완료");
+    database: process.env.DB_NAME,
+    waitForConnections: true,
+    connectionLimit: 10,
+    queueLimit: 0
 });
 
 module.exports = db;
